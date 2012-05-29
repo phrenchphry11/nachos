a4b16ea8efc06ef9200783209e5c9f9af1e42106
holly <hollyafrench90@gmail.com>
2012-05-30 01:15:59 +0200

    added connection read

--- userprog/Connection.java
+++ userprog/Connection.java
@@ -20,4 +20,19 @@ public class Connection extends OpenFile{
 		this.dstPort = dstPort;
 		this.curSeqNum = 0;
 	}
+
+	public int read(byte[] buffer, int offset, int length) {
+		int numBytesRead = 0;
+		NetMessage message = NetKernel.postOffice.receive(srcPort);
+		if (message == null) {
+			return 0;
+		}
+		for(int i = 0; i < length; i ++) {
+			buffer[i] = message.contents[i + offset];
+			numBytesRead++;
+		}
+		
+		return numBytesRead;
+
+	}
 }
